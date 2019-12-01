5202662277ef6200632a26a6d27332da86d2b3db
Tri Chandra <tri.chandra@outlook.com>
2019-12-01 21:12:29 +1100

    First working draft

--- src/RaceViewer.js
+++ src/RaceViewer.js
@@ -1,14 +1,49 @@
 import React from 'react';
+import axios from 'axios';
 
 class RaceViewer extends React.Component {
     constructor(props) {
         super(props);
 
-		this.state = { };
+		this.state = {
+            races: []
+        };
+
+        this.interval = 15000;
+        this.fetchData = this.fetchData.bind(this);
+    }
+
+    async componentDidMount() {
+        this.fetchData();
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timer);
+    }
+
+    async fetchData() {
+        const data = await axios.get('https://s3-ap-southeast-2.amazonaws.com/bet-easy-code-challenge/next-to-jump');
+        const races = data.data.result;
+
+        this.timer = setTimeout(this.fetchData, this.interval);
+// console.log(races)
+        this.setState({races});
     }
 
     render() {
-        return (<div>VIEWER</div>);
+        return (
+            <div>
+                {this.state.races.map(
+                    race => (<div key={race.EventID}>
+                        <span>{race.EventName}</span>
+                        <span>{race.Venue.Venue}</span>
+                        <span>{race.AdvertisedStartTime}</span>
+                        <span>{race.EventTypeDesc}</span>
+                    </div>)
+                )}
+                
+            </div>
+        );
     }
 }
 
