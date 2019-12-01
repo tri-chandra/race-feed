3736a3661e72c5a252b7aa0ffd3e8952e7f1446b
Tri Chandra <tri.chandra@outlook.com>
2019-12-01 20:57:19 +1100

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import RaceViewer from './RaceViewer';
+
+function App() {
+  return (
+    <div className="App">
+      <RaceViewer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/RaceViewer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class RaceViewer extends React.Component {
+    constructor(props) {
+        super(props);
+
+		this.state = { };
+    }
+
+    render() {
+        return (<div>VIEWER</div>);
+    }
+}
+
+export default RaceViewer;
